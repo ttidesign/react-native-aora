779351b9758a24bafa498db2368e85097f840d5e
TK Nguyen <nguyen_kt_2010@yahoo.com>
2024-05-19 16:37:29 -0700

    add home page content, add search component

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,12 +1,66 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { FlatList, View, Text, Image, RefreshControl } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { images } from '../../constants';
+import SearchInput from '../../components/SearchInput';
+import Trending from '../../components/Trending';
+import EmptyState from '../../components/EmptyState';
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  );
+	const [refreshing, setRefreshing] = useState(false);
+
+	const onRefresh = async () => {
+		setRefreshing(true);
+		//check if there new video
+		setRefreshing(false);
+	};
+	return (
+		<SafeAreaView className='bg-primary h-full'>
+			<FlatList
+				data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+				keyExtractor={(item) => item.$id}
+				renderItem={({ item }) => (
+					<Text className='text-3xl text-white'>{item.id}</Text>
+				)}
+				ListHeaderComponent={() => (
+					<View className='my-6 px-4 space-y-6'>
+						<View className='justify-between items-start flex-row mb-6'>
+							<View>
+								<Text className='font-pmedium text-sm text-gray-100'>
+									Welcome Back
+								</Text>
+								<Text className='text-2xl font-psemibold text-white'>User</Text>
+							</View>
+							<View className='mt-1 5'>
+								<Image
+									source={images.logoSmall}
+									className='w-9 h-10'
+									resizeMode='contain'
+								/>
+							</View>
+						</View>
+						<SearchInput />
+						<View className='w-full flex-1 pt-5 pb-8'>
+							<Text className='font-pregular text-gray-100 mb-3'>
+								Lates Video
+							</Text>
+							<Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
+						</View>
+					</View>
+				)}
+				ListEmptyComponent={() => (
+					<EmptyState
+						className='text-white'
+						title='No Videos Found'
+						subtitle='Be the first one to upload a video'
+					/>
+				)}
+				refreshControl={
+					<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+				}
+			/>
+		</SafeAreaView>
+	);
 };
 
 export default Home;
