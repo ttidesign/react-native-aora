12ee70ddc558a4095e0bb0999982f0505e6df087
TK Nguyen <nguyen_kt_2010@yahoo.com>
2024-05-18 18:59:52 -0700

    add nagivation tabs

--- app/index.jsx
+++ app/index.jsx
@@ -7,7 +7,7 @@ export default function App() {
 		<View className='flex-1 items-center justify-center bg-white'>
 			<Text className='text-3xl font-pblack'>Aora</Text>
 			<StatusBar style='auto' />
-			<Link href='/profile'> Go to profile</Link>
+			<Link href='/home'> Go to home</Link>
 		</View>
 	);
 }
